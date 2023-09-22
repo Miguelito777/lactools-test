034fdf09cd3254eb0657c364eabe11a82e4a5971
CI\MMenchu <mmenchu@compassion.com>
2023-09-22 13:17:02 -0600

    start Laravel Breeze api personalization

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try { 
+            $data = User::get();
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try { 
+            $data['name'] = $request['name'];
+            User::find($id)->update($data);
+            $res = User::find($id);
+            return response()->json( $res , 200);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
